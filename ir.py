b9193286ca890979dda1d7866619edb028893cbd
Renzo Quispe <41658080+locoxelmillo@users.noreply.github.com>
2020-09-14 09:14:35 -0300

    master

--- ir.py
+++ ir.py
@@ -2,12 +2,14 @@ import RPi.GPIO as GPIO
 import time
 import serial
 
+#Configuration GPIO of RaspberryPI3
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN)
 
+#Configuration initial port Serial
 ser = serial.Serial(
-    port = '/dev/ttyUSB0',
+    port = '/dev/ttyUSB0',             #Position initial port USB
     baudrate=9600,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
@@ -15,57 +17,30 @@ ser = serial.Serial(
     timeout=1
 )
 
-CH5   = "\xAA\xA7\xBF\x05"
-CH3   = "\xAA\xA7\xBF\x03"
-AV    = "\xAA\xA7\xFC\x05"
-HDMI  = "\xAA\xA7\xFC\x52"
-HDMI1 = "\xAA\xA7\x0F\x0F"
-HDMI2 = "\xAA\xA7\x0F\x00"
-HDMI3 = "\xAA\xA7\xBF\x03"
-ATV   = "\xAA\xA7\xBF\x1B"
-DTV   = "\xAA\xA7\xF4\x0B"
-data1 = AV
-data2 = ATV
-data3 = HDMI
+#Channel
+CH5   = "\xAA\xA7\xBF\x05"  #Code channel 5
+CH3   = "\xAA\xA7\xBF\x03"  #Code channel 3
+AV    = "\xAA\xA7\xFC\x05"  #Code AV
+HDMI  = "\xAA\xA7\xFC\x52"  #Code change channel HDMI
+HDMI1 = "\xAA\xA7\x0F\x0F"  #Code HDMI1
+HDMI2 = "\xAA\xA7\x0F\x00"  #Code HDMI2
+HDMI3 = "\xAA\xA7\xBF\x03"  #Code HDMI3
+ATV   = "\xAA\xA7\xBF\x1B"  #Code ATV
+DTV   = "\xAA\xA7\xF4\x0B"  #Code DTV
+data1 = AV                  #Variable1 reemplace
+data2 = ATV                 #Variable2 reemplace
+data3 = HDMI                #Variable3 reemplace
 
 
 while 1:
         if GPIO.input(4)==1:
-            print("AV")
+            print("AV")              #test witness
             ser.write(data1)
             time.sleep(0.5)
-            ser.write(data1)
+            ser.write(data2)
             time.sleep(0.5)        
             time.sleep(5)
         
         
         
-        
-        
-        # print("ATV")
-        # ser.write(data2)
-        # time.sleep(0.5)
-        # ser.write(data2)
-        # time.sleep(0.5)        
-        # time.sleep(5)
-        # print("HDMI")
-        # ser.write(data3)
-        # time.sleep(0.5)
-        # ser.write(data3)
-        # time.sleep(0.5)        
-        
-        
-        
-        # if GPIO.input(4)==1:
-            # print("Sensor on PRENDIDO")
-            # ser.write(data1)
-            # time.sleep(0.5)
-            # ser.write(data1)
-            # time.sleep(0.5)
-        # elif GPIO.input(4)==0:
-            # print("Sensor off")
-            # ser.write(data2)
-            # time.sleep(0.5)
-            # ser.write(data2)
-            # time.sleep(0.5)
 
